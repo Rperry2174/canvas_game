239472c9c22ef87c77ee208bcf7a86d2be0932d1
Ryan Perry <rperry2174@gmail.com>
2020-03-08 21:41:45 -0700

    Add small update

--- Assets/Scripts/Pulse.cs
+++ Assets/Scripts/Pulse.cs
@@ -7,7 +7,7 @@ public class Pulse : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        LeanTween.scale(gameObject, new Vector3(1.1f, 1.1f, 1f), 1.0f).setEase(LeanTweenType.easeInQuad).setLoopPingPong();
+        LeanTween.scale(gameObject, new Vector3(1.1f, 1.1f, 1f), 0.75f).setEase(LeanTweenType.easeInQuad).setLoopPingPong();
     }
 
     // Update is called once per frame
