95531612897c68b9b2d128db31c783bfbb2d3901
Ryan Perry <rperry2174@gmail.com>
2020-03-08 21:39:07 -0700

    Add a pulsing script to the tap to begin text button

---
+++ Assets/Scripts/Pulse.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pulse : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        LeanTween.scale(gameObject, new Vector3(1.1f, 1.1f, 1f), 1.0f).setEase(LeanTweenType.easeInQuad).setLoopPingPong();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
